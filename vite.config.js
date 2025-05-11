e5374197f04c2a1866e558a930112d2d18dc3ffc
Paul Marshall <paul@axongarside.com>
2025-05-11 22:52:36 +0100

    Change output directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/CharityBikeRide/'
+  base: '/CharityBikeRide/',
+  build: {
+    outDir: 'docs'
+  }
 })
