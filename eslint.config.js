05e89ac17a12795271dae26974dcdf694e92ae96
Paul Marshall <pw.marshall@btinternet.com>
2025-05-19 21:46:57 +0100

    Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,40 @@
-import eslint from 'eslint'
 import vuePlugin from 'eslint-plugin-vue'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default [
   {
-    files: ['**/*.{js,vue}'],
+    files: ['**/*.vue'],
+    plugins: {
+      vue: vuePlugin,
+      prettier: prettier
+    },
     languageOptions: {
+      parser: vuePlugin.parserVue,
       ecmaVersion: 'latest',
       sourceType: 'module'
-    },
-    plugins: {
-      vue: vuePlugin
     },
     rules: {
       'no-console': 'warn',
       'no-debugger': 'warn',
-      'vue/multi-word-component-names': 'off'
+      'vue/multi-word-component-names': 'off',
+      'prettier/prettier': 'error'
     }
-  }
+  },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      prettier: prettier
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'prettier/prettier': 'error'
+    }
+  },
+  prettierConfig
 ]
